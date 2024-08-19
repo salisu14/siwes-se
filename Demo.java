8710856f638897081b7a269cbf25da02cac10394
Muhammad S. Ali <salisu14@yahoo.com>
2024-08-19 14:56:04 +0100

    Create a two-dimensional array with values.

--- Demo.java
+++ Demo.java
@@ -15,12 +15,12 @@ public class Demo {
 
 
         for(int i = 1; i < 10; i++) {
-        for(int j = 1; j < i; j++) {
-                System.out.print(i + " ");
+            for(int j = 1; j < i; j++) {
+                    System.out.print(i + " ");
+            }
+        
+            System.out.println();
         }
-        System.out.println();
-        }
-
 
         var khadija = new Person("Khadija", 22);
 
@@ -29,5 +29,22 @@ public class Demo {
         sc.close();
 
         System.out.println("Welcome to Github");
+
+        System.out.println("------------ ARRAY DEMO ------------");
+
+        double[][] stats = {
+            {12.12, 23.0, 32.93},
+            {13.16, 22.0, 32.93},
+            {14.17, 21.0, 32.93},
+            {11.13, 33.0, 32.93},
+        };
+
+        for(double[] ss : stats) {
+            for(double s : ss) {
+                System.out.print(s + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
