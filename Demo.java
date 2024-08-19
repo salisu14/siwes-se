eae01f7dd8323dbd61e923f1f4382d364bdddf3f
Muhammad S. Ali <salisu14@yahoo.com>
2024-08-19 13:28:34 +0100

    Close sc object.

--- Demo.java
+++ Demo.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 
 public class Demo {
-     
+    
     record Person(String name, int age) {}
-	
+
     public static void main(String... args) {
-       //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-       Scanner sc = new Scanner(System.in);
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
-       System.out.print("Enter your name: ");
-       String name = sc.nextLine();
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
 
-       System.out.printf("Your name is %s\n", name);
-
-
-       for(int i = 1; i < 10; i++) {
-	  for(int j = 1; j < i; j++) {
-              System.out.print(i + " ");
-	  }
-	  System.out.println();
-       }
+        System.out.printf("Your name is %s\n", name);
 
 
-       var khadija = new Person("Khadija", 22);
+        for(int i = 1; i < 10; i++) {
+        for(int j = 1; j < i; j++) {
+                System.out.print(i + " ");
+        }
+        System.out.println();
+        }
 
-       System.out.println(khadija.toString());
 
+        var khadija = new Person("Khadija", 22);
+
+        System.out.println(khadija.toString());
+
+        sc.close();
     }
 }
